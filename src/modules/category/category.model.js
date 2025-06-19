10b87791c0ccbe980c88fd73377f6904f388f3cf
UDatt28-11 <datutdph49742@gmail.com>
2025-06-19 10:49:03 +0700

    refactoy/tree-folder : done

--- src/modules/category/category.model.js
+++ src/modules/category/category.model.js
@@ -23,4 +23,6 @@ const categorySchema = new Schema(
   { versionKey: false, timestamps: true }
 );
 
-export default mongoose.model("Category", categorySchema);
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
