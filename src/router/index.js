0eb7865e9945ea6ff4479b0620feb241ee958c36
UDatt28-11 <datutdph49742@gmail.com>
2025-06-19 23:06:31 +0700

    feat/sub-category -- done

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 
 import productRotes from "../modules/product/product.routes.js";
 import categoryRoutes from "../modules/category/category.routes.js";
+import subCategoryRoutes from "./../modules/subcategory/subcategory.routes";
 const router = Router();
 
 router.use("/products", productRotes);
 router.use("/categories", categoryRoutes);
+router.use("/sub-categories", subCategoryRoutes);
 
 export default router;
